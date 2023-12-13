da2aadf62dffa0edecf387ccbd9b7975ac41d21a
Millefeuille <mlabouri@student.42.fr>
2023-12-13 18:32:09 +0100

    feat: started ft_onion

---
+++ src/02_ft_onion/src/init.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+cp -n /default/torrc /etc/tor/torrc
+cp -n /default/nginx.conf /etc/nginx/nginx.conf
+cp -n /default/index.html /var/tor/lib/hidden/webserv/index.html
+
+nginx -g "daemon off;" &
+PID1=$!
+tor &
+PID2=$!
+/usr/sbin/sshd -De &
+PID3=$!
+
+check_services() {
+    if ! kill -0 "$PID1" >/dev/null 2>&1 || ! kill -0 "$PID2" >/dev/null 2>&1 || ! kill -0 "$PID3" >/dev/null 2>&1; then
+        echo "One of the services has exited. Stopping the container."
+        exit 1
+    fi
+}
+
+sleep 0.2
+
+printf "
+##########################################################################
+ _		       _____________________
+| |		      | TOR services online |
+| |_ ___  _ __	      | ------------------- |
+| __/ _ \| '__|	      |	ssh:  4242	    |
+| || (_) | |	      | http: 80            |
+ \__\___/|_|          |_____________________|
+
+> %s <
+##########################################################################
+" `cat hostname`
+
+while true; do
+    sleep 1
+    check_services
+done
